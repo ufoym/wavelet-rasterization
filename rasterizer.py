e05367afa54a34d37efb9de126a6a45b9df12f1c
Ming <m@ufoym.com>
2013-05-18 16:18:13 +0800

    clean-ups

--- rasterizer.py
+++ rasterizer.py
@@ -1,66 +1,76 @@
-import Polygon, cv2, numpy as np, time
+import Polygon, cv2, numpy as np, time, math
 from collections import namedtuple
 
-w, h, z = 8, 8, 30
-poly = ((1,2), (w-1,1), (w-1,h-1))
-
+# ------------------------------------------------------------------------------
 Point = namedtuple('Point', 'x y')
-lattice = [Point(x,y) for x in xrange(w) for y in xrange(h)]
 
-def normalize_point(p):
-	return (p[0]/float(w), p[1]/float(h))
-normed_poly = Polygon.Polygon([normalize_point(p) for p in poly])
-normed_box = [Point(*normalize_point(p)) for p in [(0,0), (1,0), (1,1), (0,1)]]
-normed_lattice = [Point(*normalize_point(p)) for p in lattice]
+class Rasterizer:
+
+    def __init__(self, poly, w, h):
+        def normalize(p):
+            return (p[0]/float(w), p[1]/float(h))
+        box = [(0,0), (1,0), (1,1), (0,1)]
+        lattice = [Point(x,y) for x in xrange(w) for y in xrange(h)]
+        self._poly = Polygon.Polygon([normalize(p) for p in poly])
+        self._box = [Point(*normalize(p)) for p in box]
+        self._lattice = [Point(*normalize(p)) for p in lattice]
+        self._w = w
+        self._h = h
+
+    def _psi(self, p, e, j, k):
+        def psi_1d(p, e):
+            if e == 0:   return 1 if 0 <= p < 1 else 0
+            else:        return (1 if 0 <= p < 0.5 else -1) if 0 <= p < 1 else 0 
+        return 2**j * psi_1d(2**j*p.x-k.x, e.x) * psi_1d(2**j*p.y-k.y, e.y)
+
+    def _c(self, e, j, k):
+        s = 0
+        for p in self._lattice:
+            psi_p = self._psi(p, e, j, k)
+            if psi_p != 0:
+                box = Polygon.Polygon([(b.x+p.x, b.y+p.y) for b in self._box])
+                px_poly = box & self._poly
+                s += px_poly.area() * psi_p
+        return s
+
+    def _g(self, p):
+        s = 0
+        # ----------------------------------------------------------------------
+        j = 0
+        for kx in xrange(2**j):
+            for ky in xrange(2**j):
+                k = Point(kx, ky)
+                s += self._c(Point(0,0), j,k) * self._psi(p, Point(0,0), j,k)
+        # ----------------------------------------------------------------------
+        max_j = int(math.ceil(math.log(max(w,h), 2)))
+        for j in xrange(max_j+1):
+            for kx in xrange(2**j):
+                for ky in xrange(2**j):
+                    k = Point(kx, ky)
+                    for e in [Point(0,1), Point(1,0), Point(1,1)]:
+                        s += self._c(e, j, k) * self._psi(p, e, j, k)
+        return s
+
+    def get(self):
+        return np.reshape([self._g(p) for p in self._lattice],(self._w,self._h))
 
 # ------------------------------------------------------------------------------
 
-def psi_2d(p, e, j, k):
-	def psi_1d(p, e):
-		if e == 0:	return 1 if 0 <= p < 1 else 0
-		else:		return (1 if 0 <= p < 0.5 else -1) if 0 <= p < 1 else 0 
-	return 2**j * psi_1d(2**j*p.x-k.x, e.x) * psi_1d(2**j*p.y-k.y, e.y)
+if __name__ == '__main__':
+    w, h, z = 8, 8, 30
+    poly = ((1,2), (w-1,1), (w-1,h-1))
 
-def c(e, j, k):
-	s = 0
-	for p in normed_lattice:
-		psi_p = psi_2d(p, e, j, k)
-		if psi_p != 0:
-			box = Polygon.Polygon([(b.x+p.x, b.y+p.y) for b in normed_box])
-			px_poly = box & normed_poly
-			s += px_poly.area() * psi_p
-	return s
+    ts = time.time()
+    raster = Rasterizer(poly, w, h).get()
+    print 'time: %2.1fs' % (time.time()-ts)
 
-def g(p, d):
-	s = 0
-	# --------------------------------------------------------------------------
-	j = 0
-	for kx in xrange(2**j):
-		for ky in xrange(2**j):
-			k = Point(kx, ky)
-			s += c(Point(0,0), j, k) * psi_2d(p, Point(0,0), j, k)
-	# --------------------------------------------------------------------------
-	for j in xrange(d):
-		for kx in xrange(2**j):
-			for ky in xrange(2**j):
-				k = Point(kx, ky)
-				for e in [Point(0,1), Point(1,0), Point(1,1)]:
-					s += c(e, j, k) * psi_2d(p, e, j, k)
-	return s
+    raster = np.array(np.asarray(raster)*30, np.uint8)
+    raster = cv2.cvtColor(raster, cv2.COLOR_GRAY2BGR)
+    raster = cv2.resize(raster, (w*z,h*z), interpolation = cv2.INTER_NEAREST)
 
-# ------------------------------------------------------------------------------
-ts = time.time()
-raster = [g(p, 4) for p in normed_lattice]
-print 'time: %2.1fs' % (time.time()-ts)
+    poly_vis = np.array([(p[1]*z, p[0]*z) for p in poly])
+    cv2.polylines(raster, [poly_vis],True,(255,255,0),lineType=cv2.CV_AA)
+    cv2.namedWindow('raster')
+    cv2.imshow('raster', raster)
 
-raster = np.array(np.asarray(raster)*30, np.uint8)
-raster = np.reshape(raster, (w,h))
-raster = cv2.cvtColor(raster, cv2.COLOR_GRAY2BGR)
-raster = cv2.resize(raster, (w*z,h*z), interpolation = cv2.INTER_NEAREST)
-
-poly_vis = np.array([(p[1]*z, p[0]*z) for p in poly])
-cv2.polylines(raster, [poly_vis],True,(255,255,0),lineType=cv2.CV_AA)
-cv2.namedWindow('raster')
-cv2.imshow('raster', raster)
-
-cv2.waitKey(0)
+    cv2.waitKey(0)
