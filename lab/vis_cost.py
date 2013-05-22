a9f38f6931d4050923a7ea9b426940d585b9e975
Ming <m@ufoym.com>
2013-05-22 18:07:24 +0800

    better visualization of likelihood cost

--- lab/vis_cost.py
+++ lab/vis_cost.py
@@ -18,24 +18,16 @@ if __name__ == '__main__':
     w, h = 800, 800
     color = Color(255, 0, 0)
 
-    # vis = np.zeros((h,w,3), np.uint8)
-    # bezier = Bezier(w*0.25,h*0.25, w*0.5,h*0.25, w*0.75,h*0.5, w*0.75,h*0.75)
-    # for i in xrange(4):
-    #     cv2.circle(vis, (int(bezier[i*2]), int(bezier[i*2+1])), 3, (255,255,0))
-    # tmp = np.zeros((h,w,4), np.uint8)
-    # tmp[:,:,:3] = vis
-    # vis = tmp
-
-
     org_img = np.zeros((h,w,4), np.uint8)
     bezier = Bezier(w*0.25,h*0.25, w*0.5,h*0.25, w*0.75,h*0.5, w*0.75,h*0.75)
     rasterize(org_img, bezier, color, w, h)
 
-    sample_num = 200
+    sample_num = 100
+    x0, y0 = bezier[2:4]
     cost_img = np.zeros((sample_num,sample_num), np.float)
     X, Y = [], []
-    for j, x in enumerate(np.linspace(0, w, num=sample_num)):
-        for i, y in enumerate(np.linspace(0, h, num=sample_num)):
+    for j, x in enumerate(np.linspace(x0-1, x0+1, num=sample_num)):
+        for i, y in enumerate(np.linspace(y0-1, y0+1, num=sample_num)):
             cur_img = np.zeros((h,w,4), np.uint8)
             bezier = Bezier(w*0.25,h*0.25, x,y, w*0.75,h*0.5, w*0.75,h*0.75)
             rasterize(cur_img, bezier, color, w, h)
@@ -49,13 +41,9 @@ if __name__ == '__main__':
     import matplotlib.pyplot as plt
     fig = plt.figure()
     ax = fig.gca(projection='3d')
-    ax.plot_surface(X, Y, cost_img, cmap='autumn', cstride=2, rstride=2)
+    ax.plot_surface(X, Y, cost_img,rstride=1, cstride=1,
+        linewidth=0, antialiased=True, alpha=0.5, cmap='autumn')
     ax.set_xlabel("x")
     ax.set_ylabel("y")
     ax.set_zlabel("cost")
     plt.show()
-
-    # vis = vis[:,:,:3]
-    # cv2.namedWindow('result')
-    # cv2.imshow('result', vis)
-    # cv2.waitKey(0)
