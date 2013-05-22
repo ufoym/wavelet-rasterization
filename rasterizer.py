00ac9b0635c6d114423949c1dd8938a0d33fa033
Ming <m@ufoym.com>
2013-05-22 22:55:57 +0800

    clean-ups

--- rasterizer.py
+++ rasterizer.py
@@ -1,25 +1,31 @@
-import Polygon, numpy as np, math
-from collections import namedtuple
+import math, collections
 from util.LiangBarsky import LiangBarsky
 
 # -----------------------------------------------------------------------------
-Point = namedtuple('Point', 'x y')
+Point = collections.namedtuple('Point', 'x y')
+def area(poly):
+    return 0.5 * sum(x0*y1 - x1*y0 \
+        for ((x0, y0), (x1, y1)) in zip(poly, poly[1:] + [poly[0]]))
 
 class Rasterizer:
 
     def __init__(self, poly, w, h):
         def normalize(p):
             return (p[0]/float(w), p[1]/float(h))
-        box = [(0,0), (1,0), (1,1), (0,1)]
-        lattice = [Point(x,y) for x in xrange(w) for y in xrange(h)]
-        self._poly = Polygon.Polygon([normalize(p) for p in poly])
-        self._normed_poly = [normalize(p) for p in poly]
-        self._box = [Point(*normalize(p)) for p in box]
-        self._lattice = [Point(*normalize(p)) for p in lattice]
         self._w = w
         self._h = h
+        self._poly = [normalize(p) for p in poly]
+        self._area = area(self._poly)
+        self._lattice = [Point(*normalize((x,y))) \
+                        for x in xrange(w) for y in xrange(h)]
+        # prepare all c
         self._all_c = {}
-        self._get_all_c()
+        max_j = int(math.ceil(math.log(max(w,h), 2)))-1
+        for j in xrange(max_j+1):
+            for kx in xrange(2**j):
+                for ky in xrange(2**j):
+                    k = Point(kx, ky)
+                    self._all_c[(j,k)] = self._c(j, k)
 
     def _psi(self, p, e, j, k):
         def psi_1d(p, e):
@@ -49,8 +55,8 @@ class Rasterizer:
         Q_00, Q_01 = Point(0, 0), Point(0, 1)
         Q_10, Q_11 = Point(1, 0), Point(1, 1)
         c10, c01, c11 = 0, 0, 0
-        for i, v1 in enumerate(self._normed_poly):
-            v0 = self._normed_poly[i-1]
+        for i, v1 in enumerate(self._poly):
+            v0 = self._poly[i-1]
             KQ00, LQ00 = get_KL(v0, v1, Q_00)
             KQ01, LQ01 = get_KL(v0, v1, Q_01)
             KQ10, LQ10 = get_KL(v0, v1, Q_10)
@@ -63,42 +69,16 @@ class Rasterizer:
                  - LQ10.x - KQ11.x + LQ11.x
         return c01, c10, c11
 
-    def _c00(self):
-        s = 0
-        e = Point(0,0)
-        j = 0
-        k = Point(0,0)
-        for p in self._lattice:
-            psi_p = self._psi(p, e, j, k)
-            if psi_p != 0:
-                box = Polygon.Polygon([(b.x+p.x, b.y+p.y) for b in self._box])
-                px_poly = box & self._poly
-                s += px_poly.area() * psi_p
-        return s
-
-    def _get_all_c(self):
-        max_wh = max(w,h)
-        max_j = int(math.ceil(math.log(max_wh, 2)))-1
-        for j in xrange(max_j+1):
-            for kx in xrange(2**j):
-                for ky in xrange(2**j):
-                    k = Point(kx, ky)
-                    self._all_c[(j,k)] = self._c(j, k)
-
     def _g(self, p):
-        s = 0
-        E_pi = [Point(0,0), Point(0,1), Point(1,0), Point(1,1)]
-        # ---------------------------------------------------------------------
-        s += self._c00() * self._psi(p, E_pi[0], 0, Point(0,0))
-        # ---------------------------------------------------------------------
-        max_wh = max(w,h)
-        max_j = int(math.ceil(math.log(max_wh, 2)))-1
+        s = self._area
+        E = [Point(0,1), Point(1,0), Point(1,1)]
+        max_j = int(math.ceil(math.log(max(w,h), 2)))-1
         for j in xrange(max_j+1):
             for kx in xrange(2**j):
                 for ky in xrange(2**j):
                     k = Point(kx, ky)
                     cs = self._all_c[(j,k)]
-                    for i, e in enumerate(E_pi[1:]):
+                    for i, e in enumerate(E):
                         psi = self._psi(p, e, j, k)
                         if psi > 0: sign = 1
                         elif psi < 0: sign = -1
@@ -108,18 +88,15 @@ class Rasterizer:
 
     def get(self):
         px_arr = [self._g(p) for p in self._lattice]
-        return np.reshape(px_arr, (self._w, self._h))
+        px_mat = [px_arr[i*w : (i+1)*w] for i in xrange(h)]
+        return px_mat
 
 # -----------------------------------------------------------------------------
 
 if __name__ == '__main__':
-    import cv2, time
+    import cv2, time, numpy as np
     from random import randint
     w, h, z = 8, 8, 30
-
-    def area(poly):
-        return 0.5 * sum(x0*y1 - x1*y0 \
-            for ((x0, y0), (x1, y1)) in zip(poly, poly[1:] + [poly[0]]))
 
     while True:
         poly = [(randint(1,w-1), randint(1,w-1)) for i in xrange(3)]
@@ -130,7 +107,7 @@ if __name__ == '__main__':
         raster = Rasterizer(poly, w, h).get()
         print '%s\ttime: %2.1fs' % (poly, time.time()-ts)
 
-        raster = np.array(raster*255+0.5, np.uint8)
+        raster = np.array(np.asarray(raster)*255+0.5, np.uint8)
         raster = cv2.cvtColor(raster, cv2.COLOR_GRAY2BGR)
         raster = cv2.resize(raster, (w*z,h*z), interpolation=cv2.INTER_NEAREST)
 
