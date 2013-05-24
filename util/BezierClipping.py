38bb60ed95d200663daca36f12c6aee5095cc65b
Ming <m@ufoym.com>
2013-05-24 23:00:04 +0800

    use a slow but robust cubic solver instead

--- util/BezierClipping.py
+++ util/BezierClipping.py
@@ -1,5 +1,6 @@
 import math, cmath
 from collections import namedtuple
+from sympy import roots
 
 # -----------------------------------------------------------------------------
 Bezier = namedtuple('Bezier', 'x0 y0 x1 y1 x2 y2 x3 y3')
@@ -14,54 +15,8 @@ def find_intersections(left, right, bottom, top, bez):
     def is_t_in(t, eps = 1e-5):
         pt = evaluate(bez, t)
         return left-eps <=pt[0]<= right+eps and top-eps <=pt[1]<= bottom+eps
-    def find_cubic_root(a, b, c, d=None):
-        ''' 
-        solve ax^3 + bx^2 + cx + d = 0 
-
-        reference
-        =========
-            http://www.physics.rutgers.edu/~masud
-            /computing/WPark_recipes_in_python.html
-        '''
-        def quadratic(a, b, c=None, eps = 1e-4):
-            ''' solve x^2 + ax + b = 0  (or ax^2 + bx + c = 0) '''
-            if a == 0:
-                if c:
-                    if b == 0:  return None, None
-                    else:       return -c/float(b), None
-                else:
-                    if b <= 0:  return (-b)**.5, -(-b)**.5
-                    elif b<eps: return 0, 0
-                    else:       return None, None
-            if c:   a, b = b / float(a), c / float(a)
-            t = a / 2.0
-            r = t**2 - b
-            if r >= 0:  y1 = math.sqrt(r)
-            else:       y1 = cmath.sqrt(r)
-            y2 = -y1
-            return y1 - t, y2 - t
-        def cbrt(x):
-            if x >= 0:  return math.pow(x, 1.0/3.0)
-            else:       return -math.pow(abs(x), 1.0/3.0)
-        def polar(x, y, deg=0):     # radian if deg=0; degree if deg=1
-            if deg:     return math.hypot(x,y), 180.0 * math.atan2(y,x)/math.pi
-            else:       return math.hypot(x,y), math.atan2(y,x)
-        if a == 0:
-            y1, y2 = quadratic(b, c, d)
-            return y1, y2, None
-        a = float(a)
-        if d:           # (ax^3 + bx^2 + cx + d = 0)
-            a, b, c = b / a, c / a, d / a
-        t = a / 3.0
-        p, q = b - 3 * t**2, c - b * t + 2 * t**3
-        u, v = quadratic(q, -(p/3.0)**3)
-        if type(u) == type(0j): # complex cubic root
-            r, w = polar(u.real, u.imag)
-            y1 = 2 * cbrt(r) * math.cos(w / 3.0)
-        else:           # real root
-            y1 = cbrt(u) + cbrt(v)
-        y2, y3 = quadratic(y1, p + y1**2)
-        return y1 - t, y2 - t, y3 - t
+    def find_cubic_root(a, b, c, d):
+        return roots('%f*x**3 + %f*x**2 + %f*x + %f' % (a, b, c, d)).keys()
     ax, bx = -bez.x0+3*bez.x1-3*bez.x2+bez.x3, 3*bez.x0-6*bez.x1+3*bez.x2
     cx, _dx = 3*bez.x1-3*bez.x0, bez.x0
     ay, by = -bez.y0+3*bez.y1-3*bez.y2+bez.y3, 3*bez.y0-6*bez.y1+3*bez.y2
